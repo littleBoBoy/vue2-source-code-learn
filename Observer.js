736a2f6bd4cec45777d6fa4f265168607ef604ee
wangzhanghao <837101207@qq.com>
2021-04-21 14:42:11 +0800

    完整版Observe和Watcher

--- Observer.js
+++ Observer.js
@@ -1,30 +1,90 @@
-function defineReactive(data, key, val) {
-  observer(val)
-  Object.defineProperty(data, key, {
-    enumerable: true,
-    configurable: true,
-    get: function () {
-      return val
-    },
-    set: function (newVal) {
-      console.log(
-        '监听到' +
-          key +
-          '由：' +
-          JSON.stringify(val) +
-          '更新为：' +
-          JSON.stringify(newVal)
-      )
-      val = newVal
+function Observer(data) {
+  this.data = data
+  this.walk(data)
+}
+
+Observer.prototype = {
+  walk: function (data) {
+    if (!data || typeof data !== 'object') return
+    Object.keys(data).forEach(key => {
+      this.defineReactive(data, key, data[key])
+    })
+  },
+  defineReactive: function (data, key, val) {
+    this.walk(val)
+    const dep = new Dep()
+    Object.defineProperty(data, key, {
+      enumerable: true,
+      configurable: true,
+      get: function () {
+        if (Dep.target) dep.addSub(Dep.target)
+        return val
+      },
+      set: function (newVal) {
+        if (newVal === val) return
+        val = newVal
+        dep.notify()
+      }
+    })
+  }
+}
+
+function Dep() {
+  this.subs = []
+  this.target = null
+}
+Dep.prototype = {
+  addSub: function (watcher) {
+    this.subs.push(watcher)
+  },
+  notify: function () {
+    this.subs.forEach(watcher => {
+      watcher.update()
+    })
+  }
+}
+
+function Watcher(vm, exp, cb) {
+  this.vm = vm
+  this.exp = exp
+  this.cb = cb
+  this.value = this.get()
+}
+
+Watcher.prototype = {
+  update: function () {
+    this.run()
+  },
+  run: function () {
+    const oldVal = this.value
+    const newVal = this._getValue()
+    if (oldVal !== newVal) {
+      this.value = newVal
+      this.cb(this.vm, newVal, oldVal)
     }
-  })
+  },
+  get: function () {
+    Dep.target = this
+    const value = this._getValue()
+    Dep.target = null
+    return value
+  },
+  _getValue() {
+    let keyChain = this.exp.split('.')
+    const oData = keyChain.reduce((pre, key, index) => {
+      if (index === keyChain.length - 1) return pre
+      else return pre[key]
+    }, this.vm.data)
+    const key = keyChain[keyChain.length - 1]
+    return oData[key]
+  }
 }
 
 function observer(data) {
-  if (!data || typeof data !== 'object') return
-  Object.keys(data).forEach(key => {
-    defineReactive(data, key, data[key])
-  })
+  return new Observer(data)
 }
 
-module.exports = observer
+module.exports = {
+  observer,
+  Watcher
+}
--- main.js
+++ main.js
@@ -1,11 +1,48 @@
-const observer = require('./Observer')
+const { observer, Watcher } = require('./Observer')
 
-const library = {
-  book1: null,
-  book2: { name: '' }
+// const library = {
+//   book1: null,
+//   book2: { name: '' }
+// }
+// observer(library)
+// new Watcher(library.book2, 'name', (vm, old, newVal) => {
+//   console.log(
+//     '我监听到了 哈哈哈哈' +
+//       'name' +
+//       '由：' +
+//       JSON.stringify(old) +
+//       '更新为：' +
+//       JSON.stringify(newVal)
+//   )
+// })
+// library.book2.name = 'asdfas'
+
+function selVue(data, el, exps) {
+  this.data = data
+  observer(this.data)
+  exps.forEach(exp => {
+    new Watcher(this, exp, (vm, newVal, old) => {
+      console.log(
+        '我监听到了 哈哈哈哈' +
+          exp +
+          '由：' +
+          JSON.stringify(old) +
+          '更新为：' +
+          JSON.stringify(newVal)
+      )
+    })
+  })
 }
 
-observer(library)
-
-library.book1 = { name: 'asdf' }
-library.book2.name = '222222'
+const app = new selVue(
+  {
+    book1: null,
+    book2: { name: '', page: 1 }
+  },
+  null,
+  ['book1', 'book2', 'book2.name', 'book2.page']
+)
+// app.data.book1 = { name: 'js基础教程', page: 80 }
+app.data.book2.name = 'css揭秘'
+app.data.book2.page = 100
+app.data.book2 = { name: 'js基础教程', page: 80 }
